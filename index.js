16aac2a539f141ad9c91e09fd0cb6e4762c1a333
ronald vilorio <soccer23415@gmail.com>
2018-05-14 17:04:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function findMatching(drivers,driver){
    return drivers.filter(function(d){return driver.toUpperCase() === d.toUpperCase()} )
 }
 function fuzzyMatch(drivers,beginning_letters){
-  return
+  return drivers.filter(function(d){return beginning_letters === d.slice(0,1)})
 }
