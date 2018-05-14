2ecd850bcbf199f967512aa2cc7229b417c7bfbe
ronald vilorio <soccer23415@gmail.com>
2018-05-14 16:56:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file
 function findMatching(drivers,driver){
-    drivers.filter(function(d){return driver === d.toUpperCase()} )
+   return drivers.filter(function(d){return driver === d.toUpperCase()} )
 }
