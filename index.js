27cbf9c56f3df69105d46cf564bc563410d87768
ronald vilorio <soccer23415@gmail.com>
2018-05-14 16:39:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file
 function findMatching(drivers, driver){
-  drivers.filter(function(name){return driver === name})
+  drivers.filter(function(driver){return driver === name})
 }
